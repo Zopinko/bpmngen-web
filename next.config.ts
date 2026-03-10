54e3a039d52afa6a958f614d37b5586ed6769170
Zopinko <peto.povazsky@email.com>
2026-03-10 10:01:47 +0100

    add canonical domain redirect bpmngen.com -> www.bpmngen.com

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "bpmngen.com",
+          },
+        ],
+        destination: "https://www.bpmngen.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
